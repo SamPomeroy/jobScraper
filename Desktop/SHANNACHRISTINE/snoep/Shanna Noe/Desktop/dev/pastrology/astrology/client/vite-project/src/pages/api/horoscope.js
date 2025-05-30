2e8f74c6d5fb91f0f1ab151ac6c763917f6359c3
Shanna Noe <snoe.prsvr@gmail.com>
2025-05-30 12:28:03 -0400

    Removed embedded repository from tracking

--- Desktop/SHANNACHRISTINE/snoep/Shanna Noe/Desktop/dev/pastrology/astrology/client/vite-project/src/pages/api/horoscope.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch("https://horoscope-d6jk8kvu9-snoecodes-projects.vercel.app/horoscopes.json");
-  const data = await response.json();
-
-  res.setHeader("Cache-Control", "no-store, max-age=0"); // Prevents caching
-  res.status(200).json(data);
-}
-
