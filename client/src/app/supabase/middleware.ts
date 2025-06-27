206af4bc865cfabcb51ebc11633298554141db98
SNoeCode <snoe.prsvr@.com>
2025-06-27 12:45:39 -0400

    made, filter, indeed and career crawlers added skills to database, sucessfully got the duplicates cleaner finished and the api skills route for the compoarion

--- client/src/app/supabase/middleware.ts
+++ client/src/app/supabase/middleware.ts
@@ -1,8 +1,23 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import type { Database } from '@/app/types/supabase';
+// import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+// import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
+// import type { Database } from '@/app/types/supabase';
 
+// export async function middleware(req: NextRequest) {
+//   const res = NextResponse.next();
+//   const supabase = createMiddlewareClient<Database>({ req, res });
+//   await supabase.auth.getSession();
+//   return res;
+// }
+
+//  export const config = {
+//   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+// };
+
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import type { Database } from "@/app/types/supabase";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({ req, res });
@@ -10,8 +25,4 @@ export async function middleware(req: NextRequest) {
   return res;
 }
 
- export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-};
-
-
+export const config = { matcher: ["/", "/dashboard", "/home"] };
